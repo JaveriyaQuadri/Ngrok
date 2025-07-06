629b0ab68d718b9b78c75220b9cfea3455635703
JaveriyaQuadri <javeriyaquadri16@gmail.com>
2025-07-06 20:51:24 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Addition addObj = new Addition();
+        Subtraction subObj = new Subtraction();
+      Multiply mulObj = new Multiply();
+
+        int num1 = 10, num2 = 5;
+
+        int sum = addObj.addition(num1, num2);
+        int diff = subObj.subtract(num1, num2);
+        int mul = mulObj.multiply(num1,num2);
+
+        System.out.println("Addition result: " + sum);
+        System.out.println("Subtraction result: " + diff);
+      System.out.println("Multiplication result: " + mul);
+    }
+}
